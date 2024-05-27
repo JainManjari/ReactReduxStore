123c62440aedb5ff4432468f849bd84787e0aad1
Manjari Jain <manjjain@paypal.com>
2024-05-27 16:41:26 +0530

    useEffect only for functional components continued

--- src/containers/UseEffect.js
+++ src/containers/UseEffect.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const UseEffect = (props) => {
   const [count, setCount] = useState(0);
+  const [data, setData] = useState(100);
 
   useEffect(() => {
     console.warn("useEffect home", count); // whenever the state or props is changes
@@ -15,13 +16,21 @@ const UseEffect = (props) => {
     console.warn("use effect for count ", count); // whenever the count value changes
   }, [count]);
 
+  useEffect(() => {
+    console.warn("use effect for count & data ", count); // whenever the count and data value changes
+  }, [count, data]);
+
   return (
     <div style={{ marginTop: 100 }}>
       <h2>Use Effect</h2>
       <button type="button" onClick={() => setCount(count + 1)}>
         Update Count
       </button>
+      <button type="button" onClick={() => setData(data + 1)}>
+        Update Data
+      </button>
       <h4>Count : {count}</h4>
+      <h4>Data : {data}</h4>
     </div>
   );
 };
